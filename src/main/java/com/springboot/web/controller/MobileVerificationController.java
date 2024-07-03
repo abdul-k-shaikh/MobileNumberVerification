b0778e11792404dc3a83504438d581c4bb83f3c8
abdul-k-shaikh <theabdul97@gmail.com>
2024-07-03 17:02:40 +0530

    removingUnusedCode

--- src/main/java/com/springboot/web/controller/MobileVerificationController.java
+++ src/main/java/com/springboot/web/controller/MobileVerificationController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.springboot.web.service.MobileVerificationService;
 
@@ -16,26 +15,6 @@ public class MobileVerificationController {
 	@Autowired
 	private MobileVerificationService mobileVerificationService;
 
-//	@RequestMapping("/")
-//	@GetMapping("/")
-//	public String home() {
-//		System.out.println("this is home page");
-//		return "home";
-//	}
-
-	@GetMapping("/")
-	public ModelAndView showIndexPage() {
-		return new ModelAndView("index");
-	}
-
-	@Controller
-	public class VeriphoneController {
-
-//		private final MobileVerificationService mobileVerificationService;
-
-//		public VeriphoneController(MobileVerificationService mobileVerificationService) {
-//			this.mobileVerificationService = mobileVerificationService;
-//		}
 
 		@GetMapping("/verify")
 		public String showVerifyPage() {
@@ -49,4 +28,3 @@ public class MobileVerificationController {
 		}
 	}
 
-}
